8c1663c12fe76a634eed3ad15e4e94af6fc65ca2
Julian Furchert <julian.furchert@raytion.com>
2020-11-23 23:51:11 +0100

    Update

--- packages/system-blocks/index.ts
+++ packages/system-blocks/index.ts
@@ -4,3 +4,6 @@ export { css, styled, theme, darkTheme } from './stitches.config';
 // Components
 export { Button } from './components/Button';
 export type { ButtonProps, ButtonVariants } from './components/Button';
+
+// Storybook 
+// export { default as ButtonStories } from './stories/Button.stories'
